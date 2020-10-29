27b303fbc0599c522f568fc78becf9f8c469a9b8
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-10-29 12:29:49 -0500

    Initial commit.

---
+++ app_as_aws_sam/layer_test/src/main/java/com/james2ch9developer/layer_test/LambdaHandler.java
@@ -0,0 +1,13 @@
+package com.james2ch9developer.layer_test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler implements RequestHandler<String, String> {
+
+  public String handleRequest(String input, Context context) {
+
+	  context.getLogger().log("Input: " + input);
+	  return "Hello World - " + input;
+  }
+}
---
+++ app_as_aws_sam/layer_test/src/main/java/com/james2ch9developer/layer_test/LayerTest.java
@@ -0,0 +1,6 @@
+package com.james2ch9developer.layer_test;
+public class LayerTest {
+  public static void main(String[] args) {
+    System.out.println("Hello world - Oct. 29, 2020");
+  }
+}
